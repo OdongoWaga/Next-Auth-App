1d80f172aedd34911871c15e632e2f4a0addaed6
Waga Odongo <wagaodongo@gmail.com>
2019-04-09 15:34:02 +0300

    LoginForm
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,23 +1,39 @@
 import { loginUser } from "../lib/auth";
+import Router from "next/router";
 
 class LoginForm extends React.Component {
 	state = {
-		email: "",
-		password: ""
+		email: "Rey.Padberg@karina.biz",
+		password: "ambrose.net",
+		error: "",
+		isLoading: false
+	};
+
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
 	};
 
 	handleSubmit = (event) => {
 		const { email, password } = this.state;
+
 		event.preventDefault();
-		loginUser(email, password);
+		this.setState({ error: "", isLoading: true });
+		loginUser(email, password)
+			.then(() => {
+				Router.push("/profile");
+			})
+			.catch(this.showError);
 	};
 
-	handleChange = (event) => {
-		this.setState({ [event.target.name]: event.taget.value });
+	showError = (err) => {
+		console.error(err);
+		const error = (err.response && err.response.data) || err.message;
+		this.setState({ error, isLoading: false });
 	};
 
 	render() {
 		const { email, password, error, isLoading } = this.state;
+
 		return (
 			<form onSubmit={this.handleSubmit}>
 				<div>
@@ -38,7 +54,10 @@ class LoginForm extends React.Component {
 						onChange={this.handleChange}
 					/>
 				</div>
-				<button type="submit"> Submit </button>
+				<button disabled={isLoading} type="submit">
+					{isLoading ? "Sending" : "Submit"}
+				</button>
+				{error && <div>{error}</div>}
 			</form>
 		);
 	}
