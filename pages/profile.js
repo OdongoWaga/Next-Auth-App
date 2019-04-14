3fee258467e0fcc0da69e5536f5873b1e8b234d8
Waga Odongo <wagaodongo@gmail.com>
2019-04-14 11:32:43 +0300

    logout
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,8 @@
-import { getUserProfile } from "../lib/auth";
+import { getUserProfile, authInitialProps } from "../lib/auth";
 import React, { Component } from "react";
 import Layout from "../components/Layout";
 
-export default class profile extends Component {
+export default class Profile extends Component {
 	state = {
 		user: null
 	};
@@ -11,9 +11,11 @@ export default class profile extends Component {
 	}
 	render() {
 		return (
-			<Layout title="Profile ">
+			<Layout title="Profile" {...this.props}>
 				<pre>{JSON.stringify(this.state.user, null, 2)}</pre>
 			</Layout>
 		);
 	}
 }
+
+Profile.getInitialProps = authInitialProps();
